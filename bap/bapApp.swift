804a4c17d3f27e462b23cec2b4a4672d2593ba18
baptiste <collebaptiste@gmail.com>
2026-01-08 11:20:22 +0000

    Initial Commit

---
+++ bap/bapApp.swift
@@ -0,0 +1,17 @@
+//
+//  bapApp.swift
+//  bap
+//
+//  Created by Baptiste Colle on 08/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct bapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
